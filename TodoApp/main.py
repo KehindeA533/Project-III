93dcc89a314bb2fb26372c8ddcf82ce7ff53ba9f
Kehinde. A <whenkehindewalks@gmail.com>
2024-05-10 17:09:00 -0400

    add get single todos endpoint

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Path, status
 from models import Todos
 import models
 from db import engine, SessionLocal
@@ -22,6 +22,16 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/Todos", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/Todos/{id}", status_code=status.HTTP_200_OK)
+async def get_single_todos(db: db_dependency, id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, details="Todo not found."
+    )
