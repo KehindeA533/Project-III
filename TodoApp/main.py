e31689c8e9f894a5f34291246f189f269f738ffb
Kehinde. A <whenkehindewalks@gmail.com>
2024-05-11 19:47:56 -0400

    add update_post endpoint

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -56,3 +56,23 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.model_dump())
     db.add(todo_model)
     db.commit()
+
+
+@app.put("/Todos/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(
+    db: db_dependency, todo_request: TodoRequest, id: int = Path(gt=0)
+):
+    todo_model = db.query(Todos).filter(Todos.id == id).first()
+
+    if todo_model is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, details="Todo not found."
+        )
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
