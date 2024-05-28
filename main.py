60f37000e5193cfca48ec22beb926e38b12ba2ed
Kehinde. A <whenkehindewalks@gmail.com>
2024-05-27 22:04:58 -0400

    Added css to templates

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 import models
 from db import engine
 from routers import auth, todos
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/healthy")
